c7d775033f8a51ff00d90b329b47fa97fff00d10
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2021-10-17 00:51:58 -0400

    simple parse

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-console.log('hi there')
-console.log('hi therdasdasde')
-console.log('hi therdasdasde')
-const obj = {a: 'a'};
-const newObj = {...obj, b: 'b'};
+const elements = document.querySelectorAll('div[data-text-gif]');
+
+const data = {
+    play: true,
+    lang: 'js',
+    screens: [
+        {
+            content: "<p>test test te ok'<p>wowo</p>'st</p>",
+            notes: 'this is just a thing'
+        }
+    ]
+}
+
+for (let i = 0; i < elements.length; i++) {
+    const element = elements[i].textContent;
+    const { play, lang, screens } = JSON.parse(atob(element));
+    console.log('data: ', play, lang, screens);
+}
